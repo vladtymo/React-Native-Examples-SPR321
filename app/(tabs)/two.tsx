61f7a7a54e1d9247d998b27c21e1fa8841843cdc
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-04-05 13:25:00 +0300

    Create forms

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,87 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { Alert, Button, StyleSheet, Switch, TextInput } from 'react-native';
 import { Text, View } from '@/components/Themed';
+import React from 'react';
+
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Picker } from '@react-native-picker/picker'
+import { useForm, Controller } from "react-hook-form"
+
+type FormData = {
+  email: string;
+  lang: string;
+  accept: boolean;
+};
 
 export default function TabTwoScreen() {
+
+  const {
+    control,
+    handleSubmit
+  } = useForm<FormData>({
+    defaultValues: {
+      email: "",
+      lang: "js",
+      accept: false,
+    },
+  })
+  const onSubmit = (data: FormData) => {
+    Alert.alert("Form Data", `Data: ${data.email} - ${data.lang}`)
+    console.log("Form Data", data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <Text style={styles.text}>Create New Product</Text>
+
+      <Text>Your email:</Text>
+      <Controller
+        control={control}
+        name="email"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            style={{ ...styles.input, ...styles.text }}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+      />
+
+      <DateTimePicker value={new Date()} mode="date" />
+      <DateTimePicker value={new Date()} mode="time" />
+
+      <Text>Select your favourite lang:</Text>
+      <Controller
+        control={control}
+        name="lang"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Picker
+            style={styles.text}
+            selectedValue={value}
+            onValueChange={onChange}>
+            <Picker.Item label="Select a language..." value="" enabled={false} />
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+            <Picker.Item label="Python" value="py" />
+            <Picker.Item label="Ruby" value="rb" />
+            <Picker.Item label="Perl" value="pl" />
+            <Picker.Item label="Kotlin" value="kot" />
+            <Picker.Item label="PHP" value="php" />
+          </Picker>
+        )}
+      />
+
+      <Text>Accept policy:</Text>
+      <Controller
+        control={control}
+        name="accept"
+        render={({ field: { onChange, onBlur, value } }) => (
+          <Switch
+            onValueChange={onChange}
+            value={value} />
+        )}
+      />
+
+      <Button title='Submit' onPress={handleSubmit(onSubmit)}></Button>
     </View>
   );
 }
@@ -18,14 +91,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 12,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  text: {
+    color: 'black',
+    fontSize: 18,
+    textAlign: 'center'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  input: {
+    backgroundColor: "lightgray",
+    padding: 10,
+    minWidth: 200,
+    color: 'white',
+    fontSize: 18,
+  }
 });
